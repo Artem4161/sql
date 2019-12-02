182bb9d0b7147517a340ab9d2b0397e70e795f76
Artem <prestige0629@gmail.com>
2019-12-02 15:18:40 +0200

    first commit

---
+++ SQL1.sql
@@ -0,0 +1,21 @@
+﻿SELECT DISTINCT
+      tmp.department,
+      tmp.parent_department,
+      tmp.cnt_users 
+  FROM (SELECT dp.name AS department,
+       pdp.name AS parent_department,
+       (SELECT count(*)  FROM users AS u
+                        WHERE u.department_id = dp.id
+       ) AS cnt_users,
+       usr.birthday
+  FROM  parent_departments AS pdp
+      LEFT OUTER JOIN departments AS dp ON dp.parent_id = pdp.id
+      LEFT OUTER JOIN users AS usr ON usr.department_id = dp.id
+  ) AS tmp
+
+  WHERE tmp.cnt_users > 1 
+        AND tmp.birthday > '01.01.1990'
+
+  ORDER BY  tmp.cnt_users,
+            tmp.parent_department ASC,
+            tmp.department ASC
---
+++ SQL2.sql
@@ -0,0 +1,19 @@
+﻿SELECT  tmp.id,
+        tmp.name,
+        (SELECT pr.price FROM price AS pr
+            WHERE pr.type_id = 1 AND pr.product_id = tmp.id
+        ) AS price_type1,
+        (SELECT pr.price FROM price AS pr
+            WHERE pr.type_id = 2 AND pr.product_id = tmp.id
+        ) AS price_type2,
+        (SELECT pr.price FROM price AS pr
+            WHERE pr.type_id = 5 AND pr.product_id = tmp.id
+        ) AS price_type5
+FROM (SELECT prod.id,
+      prod.name
+FROM products AS prod,
+      price AS pr
+WHERE prod.id = pr.product_id
+GROUP BY prod.id,
+      prod.name
+) AS tmp
